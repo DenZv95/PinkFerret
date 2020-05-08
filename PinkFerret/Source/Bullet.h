5ec079f10713aebf9e499b03dea378ba300a5382
Den_095 <den.zv95@yandex.ru>
2020-05-08 19:47:36 +0900

    Bullet Collision

--- PinkFerret/Source/Bullet.h
+++ PinkFerret/Source/Bullet.h
@@ -1,11 +1,15 @@
 #include "Entity.h"
-
+#include <Objects.h>
+#include <level.h>
 
 class Bullet : public Entity
 {
 public:
     Animation anim;
-    Bullet(Animation& a);
+    Bullet(Animation& a, Level& level);
     void draw(RenderWindow& app, float time) override;
     void update() override;
+private:
+    std::vector<Object> obj;
+    void checkCollisionWithMap(float Dx, float Dy);
 };
